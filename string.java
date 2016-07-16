76492d07407b72931fbd006ce9d766c9b04d7c24
kamalee29 <kamalee.selvii24@gmail.com>
2016-07-16 09:51:54 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,9 @@
+ if(s1.equalsIgnoreCase(s2))
+        {
+        
+               System.out.println("false");
+        }
+          else
+        {
+            System.out.println("true");       
+                }
