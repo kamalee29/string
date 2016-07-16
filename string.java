c6c0ca431469fd8b878a3dbe027634812c5844ed
kamalee29 <kamalee.selvii24@gmail.com>
2016-07-16 17:15:11 +0530

    Delete string.java

--- string.java
+++
@@ -1,9 +0,0 @@
- if(s1.equalsIgnoreCase(s2))
-        {
-        
-               System.out.println("false");
-        }
-          else
-        {
-            System.out.println("true");       
-                }
